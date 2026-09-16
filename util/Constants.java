61865f0b2eb09318c63954e63a706f1c1d5dab02
Aniruddha Majumdar <aniruddhamajumdar07@gmail.com>
2026-09-17 02:04:43 +0530

    Create Constants.java

---
+++ util/Constants.java
@@ -0,0 +1,7 @@
+package library.util;
+//constants model lol....
+public class Constants {
+    public static final int MAX_BOOKS_PER_MEMBER = 3;
+    public static final int START_BOOK_ID = 101;
+    public static final int START_MEMBER_ID = 501;
+}
